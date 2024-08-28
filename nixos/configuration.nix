33467f0aa1760311928e03cf4661b6ebfbfdbac8
Kiran PS <pskirann@gmail.com>
2024-08-28 11:49:10 +0530

    Disable openssh server

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -106,7 +106,7 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  # services.openssh.enable = true;
 
   system.stateVersion = "22.11";
 
