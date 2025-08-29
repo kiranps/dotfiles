b4f7a602051150691e63ebff29ef5edd24816c2f
Kiran PS <pskirann@gmail.com>
2025-08-29 13:08:30 +0530

    add wf-recorder

--- home/common/wayland/packages.nix
+++ home/common/wayland/packages.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     wf-recorder # Screen recorder for Wayland
+    wlr-randr # RandR implementation for wlroots-based compositors
     xdg-desktop-portal-wlr # XDG desktop portal for Wayland compositors
   ];
 }
